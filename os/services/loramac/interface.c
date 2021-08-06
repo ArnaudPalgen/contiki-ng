34e47d41061eaa799fec8ec6ae92a886b8c6f35f
ArnaudPalgen <arnaud.palgen@student.umons.ac.be>
2021-08-06 20:16:06 +0200

    fix bug ipv62lora

--- os/services/loramac/interface.c
+++ os/services/loramac/interface.c
@@ -15,28 +15,29 @@ PROCESS(loramac_process, "LoRaMAC-interface");
 Conversion function between lora_addr_t and uip_ip6addr_t.
 An IPv6 address is build as follow:
 
-|<-----1----->|<--5-->|<-----1----->|<--1-->|<----------6---------->|<---2--->|
-| IPv6 PREFIX | ZEROS | LORA PREFIX | ZEROS | COMMON_LINK_ADDR_PART | NODE_ID |
- 0           0 1     5 6           6 7     7 8                    13 14     15
+|<-----1----->|<--6-->|<-----1----->|<----------6---------->|<---2--->|
+| IPv6 PREFIX | ZEROS | LORA PREFIX | COMMON_LINK_ADDR_PART | NODE_ID |
+ 0           0 1     6 7           7 8                    13 14     15
 
 Where:
   - IPv6 PREFIX is 0xFD
   - LORA PREFIX is The prefix of the lora_addr_t
   - COMMON_LINK_ADDR_PART is the 6 first bytes of the link layer address and defined as follow: 0x02, 0x12, 0x4B, 0x00, 0x06, 0x0D
   - NODE_ID is the node-id of the node
-
 */
 
-void lora2ipv6(lora_addr_t *src_addr, uip_ip6addr_t *dest_addr)
+void lora2ipv6(lora_addr_t *src_addr, uip_ip6addr_t *dest_addr)//review
 {
   uip_ip6addr_u8(dest_addr, 0xFD, 0, 0, 0, 0, 0, src_addr->prefix, 0, 0x02, 0x12, 0x4B, 0x00, 0x06, 0x0D, src_addr->id>>8, src_addr->id);
   
 }
 
-void ipv62lora(uip_ip6addr_t *src_addr, lora_addr_t *dest_addr)
+void ipv62lora(uip_ip6addr_t *src_addr, lora_addr_t *dest_addr)//done
 {
-  dest_addr->prefix = src_addr->u8[6];
-  dest_addr->id = src_addr->u8[14] + ((src_addr->u8[15]) << 8);
+  LOG_INFO("CONVERT SRC ADDR:");
+  LOG_INFO_6ADDR(src_addr);
+  dest_addr->prefix = src_addr->u8[7];
+  dest_addr->id = (src_addr->u8[14] <<8) + src_addr->u8[15];
 }
 
 /*---------------------------------------------------------------------------*/
@@ -63,7 +64,7 @@ init(void)
  * payload to which we add 32 bytes of the src/dest IP addresses that are not used.
  */
 static int
-output(void)
+output(void)//done
 { 
 
   LOG_INFO("Receive data for loramac whouhouuuuuu\n");
@@ -118,7 +119,7 @@ PROCESS_THREAD(loramac_process, ev, data)
 
   LOG_INFO("PREFIX: %d\n", loramac_addr.prefix);
   uip_ipaddr_t prefix;
-  uip_ip6addr_u8(&prefix, 0xFD, 0, 0, 0, 0, 0, loramac_addr.prefix, 0, 0,0,0,0,0,0,0,0);
+  uip_ip6addr_u8(&prefix, 0xFD, 0, 0, 0, 0, 0, 0, loramac_addr.prefix, 0,0,0,0,0,0,0,0);
   NETSTACK_ROUTING.root_set_prefix(&prefix, NULL);
   NETSTACK_ROUTING.root_start();
   NETSTACK_MAC.on();

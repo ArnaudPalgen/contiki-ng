ae1c00d0b9e46a6e8891f19a14460a2afdc14063
ArnaudPalgen <arnaud.palgen@student.umons.ac.be>
2021-10-11 17:47:16 +0200

    begin v2 framer

---
+++ os/services/loramac/v2/framer.c
@@ -0,0 +1,151 @@
+#include "contiki.h"
+
+#ifndef LORAFRAMER_H_
+#define LORAFRAMER_H_
+
+#endif /* LORAFRAMER_H_ */
+
+/* Parse the received data to a LoRaMAC frame in the lorabuf */
+int parse(void);
+
+/* Create a string of char from the lorabuf */
+int create(void);
+
+////////////////////////////////////////////////////////
+int
+parse(void)
+{
+    char prefix_c[2];
+    char id_c[4];
+}
+
+
+/* build lora_frame_t from char* */
+int parse(lora_frame_t *dest, char *data){
+
+    if(strlen(data) < HEADER_SIZE){
+        return 1;
+    }
+    
+    lora_frame_t result;
+    
+    char prefix_c[2];
+    char id_c[4];
+    
+    uint8_t prefix;
+    uint16_t id;
+    lora_addr_t src_addr;
+    lora_addr_t dest_addr;
+
+    /*extract src addr*/
+    memcpy(prefix_c, data, 2);
+    data = data+2;
+    memcpy(id_c, data, 4);
+    data = data+4;
+
+    prefix = (uint8_t) strtol(prefix_c, NULL, 16);
+    id = (uint16_t) strtol(id_c, NULL, 16);
+
+    src_addr.prefix = prefix;
+    src_addr.id=id; 
+    result.src_addr = src_addr;
+
+    /*extract dest addr*/
+    memcpy(prefix_c, data, 2);
+    data = data+2;
+    memcpy(id_c, data, 4);
+    data = data+4;
+
+    prefix = (uint8_t) strtol(prefix_c, NULL, 16);
+    id = (uint16_t) strtol(id_c, NULL, 16);
+
+    dest_addr.prefix = prefix;
+    dest_addr.id = id;
+    result.dest_addr = dest_addr;
+
+    /*extact flags and command*/
+    char cmd[2];
+    memcpy(cmd, data, 2);
+    data = data+2;
+    uint8_t i_cmd = (uint8_t)strtol(cmd, NULL, 16);
+
+    uint8_t flag_filter = 0x01;
+    uint8_t command_filter = 0x0F;
+
+    bool k    = (bool)((i_cmd >> 7) & flag_filter);
+    bool next = (bool)((i_cmd >> 6) & flag_filter);
+    
+    mac_command_t command = (uint8_t)( i_cmd & command_filter );
+    
+    result.k = k;
+    result.command = command;
+    result.next = next;
+
+    /* extract SN */
+    char sn_c[2];
+    memcpy(sn_c, data, 2);
+    data = data+2;
+    uint8_t sn = (uint8_t)strtol(sn_c, NULL, 16);
+    result.seq = sn;
+
+    /*extract payload*/
+    result.payload = data;
+    memcpy(dest, &result, sizeof(lora_frame_t));
+    
+    return 0;
+}
+
+/*---------------------------------------------------------------------------*/
+/*convert lora_frame_t to hex*/
+int to_frame(lora_frame_t *frame, char *dest){
+
+    char result[HEADER_SIZE+PAYLOAD_MAX_SIZE]="";
+
+    /*create src and dest addr*/
+    char src_addr[6];
+    char dest_addr[6];
+    
+    sprintf(src_addr, "%02X%04X", frame->src_addr.prefix, frame->src_addr.id);
+    sprintf(dest_addr, "%02X%04X", frame->dest_addr.prefix, frame->dest_addr.id);
+    
+    /*create flags and MAC command*/
+    char flags_command[2];
+
+    uint8_t f_c = 0;
+    if(frame->k){
+        f_c = f_c | K_FLAG;
+    }
+    if(frame->next){
+        f_c = f_c | NEXT_FLAG;
+    }
+    f_c = f_c | ((uint8_t) frame->command);
+
+    sprintf(flags_command, "%02X", f_c);
+
+    /* create SN */
+    char sn[2];
+    sprintf(sn, "%02X", frame->seq);
+    
+    /* concat all computed values to result */
+    strcat(result, src_addr);
+    strcat(result, dest_addr);
+    strcat(result, flags_command);
+    strcat(result, sn);
+    
+    /* create payload */
+    int payload_size = 0;
+    if(frame->payload != NULL){
+        payload_size = strlen(frame->payload);
+    } 
+    
+    if(payload_size>0){
+        if(payload_size%2 != 0){
+            strcat(result, "0");
+        }
+        strcat(result, frame->payload);
+    }
+    
+    /*copy result to dest */
+    memcpy(dest, &result, HEADER_SIZE+payload_size+1);
+    return 0;
+}
